9bcd2dc423eb4bd57649a0a5982cbade7556cfdb
zamparg <zamparg@hotmail.com>
2023-04-28 10:52:05 -0300

    planificación JS

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+/*
+    TIRAR MONEDAS: 
+    Devuelve un valor: 6,7,8,9
+    genera objeto => tipo de línea = true o false (si es completa o abierta).
+                    mutable= true o false
+                    a los 3---> guardar trigrama (requerimos json de trigramas con id)
+                    
+    obj = {
+        trigramaInf= id
+        trigramaSup = id
+        lineas ={
+            1:true o false si es mutable
+            2:
+            3:
+        }
+    }
+
+    filtrar en data.json por trigramas y traer objeto Hexagrama. Guardar.
+    funcion mutadora. 
+    guardar en otra variable el hexagrama mutado también. 
+    mostrar en DOM con boton de mutar -> que muestre el hexagrama mutado y permita volver. 
+*/
