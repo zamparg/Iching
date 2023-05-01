6e1f9c960ddd286cb3346d285bb4abdcff1e1e51
zamparg <zamparg@hotmail.com>
2023-05-01 12:30:12 -0300

    cambios varios

--- scripts/main.js
+++ scripts/main.js
@@ -21,8 +21,10 @@
     mostrar en DOM con boton de mutar -> que muestre el hexagrama mutado y permita volver. 
 */
 
+start()
+let trigrams
+let hexagrams
 
-let numeros = [6, 7, 8, 9]
 let buttonContainer=document.getElementById('buttonContainer')
 let buttonCoins=document.getElementById('buttonCoins')
 let flipper = 1
@@ -32,7 +34,6 @@ let question = {
     'hexagram':[],
     'hexagramM':[]
 }
-
 
 function flipCoins(e) {
     let moneda1 = Math.round(Math.random() * (3 - 2) + 2) // devuelve un valor entre 2 y 3
@@ -48,6 +49,7 @@ function flipCoins(e) {
     question.hexagram.push(flip)
     if(flipper==6){
         buttonContainer.innerHTML=``
+        searchHexagram()
     }
     flipper+=1
     console.log(question)
@@ -62,7 +64,6 @@ function mutable(coin) {
         return false
     }
 }
-console.log(mutable());
 
 function typeOfLine(coin) {
     if (coin === 7 || coin === 9) {
@@ -72,4 +73,25 @@ function typeOfLine(coin) {
         return false
     }
 }
-console.log(typeOfLine());
+
+function searchHexagram(){
+    console.log(trigrams, question)
+    question.trigramInf = trigrams.trigrams.find(el => el.lines[0]== question.hexagram.line[0] &&
+        el.lines[1]== question.hexagram.line[1] &&
+        el.lines[2]== question.hexagram.line[3]
+        )
+        console.log(question)
+}
+
+async function fetchData(url){
+    let dataAsync = await fetch(url)
+        .then(response=>response.json())
+        .then(datos =>{            
+        return datos})
+    return dataAsync
+}
+
+async function start(){
+    trigrams = await fetchData('./scripts/trigrams.json');
+    hexagrams = await fetchData('./scripts/data.json')
+}
