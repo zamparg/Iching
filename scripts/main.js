2c9eb89aea5dd65838e0751cbfa81e138da33b6b
Luciano <luciano.carrizo26@gmail.com>
2023-05-03 10:06:34 -0300

    monedas

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,3 @@
-/*
-    TIRAR MONEDAS: 
-    Devuelve un valor: 6,7,8,9
-    genera objeto => tipo de línea = true o false (si es completa o abierta).
-                    mutable= true o false
-                    a los 3---> guardar trigrama (requerimos json de trigramas con id)
-                    
-    obj = {
-        trigramaInf= id
-        trigramaSup = id
-        lineas ={
-            1:true o false si es mutable
-            2:
-            3:
-        }
-    }
-
-    filtrar en data.json por trigramas y traer objeto Hexagrama. Guardar.
-    funcion mutadora. 
-    guardar en otra variable el hexagrama mutado también. 
-    mostrar en DOM con boton de mutar -> que muestre el hexagrama mutado y permita volver. 
-*/
-
 start()
 let trigrams
 let hexagrams
@@ -30,18 +7,15 @@ let buttonCoins=document.getElementById('buttonCoins')
 let flipper = 1
 buttonCoins.addEventListener('click', flipCoins)
 
-
 let question = {
     'hexagram':[],
     'hexagramM':[]
 }
 
 function flipCoins(e) {
-    let moneda1 = Math.round(Math.random() * (3 - 2) + 2) // devuelve un valor entre 2 y 3
+    let moneda1 = Math.round(Math.random() * (3 - 2) + 2)
     let moneda2 = Math.round(Math.random() * (3 - 2) + 2)
     let moneda3 = Math.round(Math.random() * (3 - 2) + 2)
-    console.log(moneda1,moneda2, moneda3)
-    // función que pinte monedas
     let sumatoria= moneda1+moneda2+moneda3
     let flip ={
         'line': typeOfLine(sumatoria),
@@ -53,9 +27,7 @@ function flipCoins(e) {
         searchHexagram()
     }
     flipper+=1
-    console.log(question)
 }
-
 
 function mutable(coin) {
     if (coin === 6 || coin === 9) {
@@ -73,7 +45,6 @@ function typeOfLine(coin) {
         return false
     }
 }
-
 
 function searchHexagram(){
     console.log(trigrams, question)
